7703eab5734126964e64db6673dc699e63afcd8f
Raaed Kabir <raaed.kabir@gmail.com>
2020-11-21 21:03:50 +0800

    updated Home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,81 +10,7 @@
         <a href="#">Learn More About Me</a>
       </div>
     </section>
-    <section class="home__works">
-      <div class="container">
-        <h2 class="heading__section">My Works <span>_</span></h2>
-        <div class="home__works--container one">
-          <img src="@/assets/images/works/motor_dashboard.png" alt="" class="home__works--image" />
-          <p class="title">Motor Dashboard</p>
-          <p class="details">
-            Made with Vue.
-          </p>
-        </div>
-        <!-- <div class="home__works--container two">
-          <img
-            src="https://source.unsplash.com/featured/?computer"
-            alt=""
-            class="home__works--image"
-          />
-          <p class="title">Lorem Ipsum</p>
-          <p class="details">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae veniam laborum sint, dolor
-            numquam nam in error minus. Distinctio tempore vero maxime veniam voluptates deserunt,
-            quod magni cupiditate officiis incidunt.
-          </p>
-        </div>
-        <div class="home__works--container three">
-          <img
-            src="https://source.unsplash.com/featured/?project"
-            alt=""
-            class="home__works--image"
-          />
-          <p class="title">Lorem Ipsum</p>
-          <p class="details">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae veniam laborum sint, dolor
-            numquam nam in error minus. Distinctio tempore vero maxime veniam voluptates deserunt,
-            quod magni cupiditate officiis incidunt.
-          </p>
-        </div> -->
-        <div class="home__works--container four">
-          <img src="@/assets/images/works/tech_site.png" alt="" class="home__works--image" />
-          <p class="title">Tech Website Design</p>
-          <p class="details">
-            Designed in Photoshop.
-          </p>
-        </div>
-        <div class="home__works--container five">
-          <a href="/project_generator_site/index.html">
-            <img
-              src="@/assets/images/works/project_generator_site.jpg"
-              alt=""
-              class="home__works--image"
-            />
-          </a>
-          <p class="title">Project Generator</p>
-          <p class="details">
-            Made in HTML & CSS.
-          </p>
-        </div>
-        <div class="home__works--container six">
-          <router-link to="/components">
-            <img
-              src="@/assets/images/works/component_library.png"
-              alt=""
-              class="home__works--image"
-            />
-          </router-link>
-          <p class="title">Pure CSS Components</p>
-        </div>
-        <div class="home__works--container seven">
-          <img src="@/assets/images/works/lorem_image.jpg" alt="" class="home__works--image" />
-          <p class="title">Lorem Ipsum</p>
-          <p class="details">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          </p>
-        </div>
-      </div>
-    </section>
+    <home-works />
     <section class="home--contact">
       <p>Have an awesome project and need a developer?</p>
       <p>Hit me up and <a href="contact.html">let's chat</a>!</p>
@@ -96,11 +22,13 @@
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 import HomeHero from '@/components/Home/Hero.vue';
+import HomeWorks from '@/components/Home/Works.vue';
 import TheFooter from '@/components/Layout/TheFooter.vue';
 
 @Options({
   components: {
     HomeHero,
+    HomeWorks,
     TheFooter
   }
 })
@@ -111,72 +39,7 @@ export default class Home extends Vue {}
 @import '@/assets/scss/abstracts/_mixins.scss';
 
 .home--about {
-  padding: 10rem 0;
-}
-
-.home__works {
-  padding-bottom: 10rem;
-  background: var(--clr-dark);
-
-  &--container > img {
-    height: 100%;
-    width: 100%;
-    object-fit: cover;
-  }
-
-  & .container {
-    display: grid;
-    gap: 2rem;
-    grid-template-columns: repeat(5, 1fr);
-
-    & h2 {
-      grid-column: 1 / -1;
-    }
-
-    & .one {
-      grid-row: 2;
-      grid-column: 1 / -1;
-    }
-
-    & .two {
-      grid-row: 3;
-      grid-column: 1 / 3;
-      height: 30rem;
-    }
-
-    & .three {
-      grid-row: 3 / 4;
-      grid-column: 3 / 6;
-    }
-
-    & .four {
-      grid-row: 4 / 5;
-      grid-column: 1 / 3;
-    }
-
-    & .five {
-      grid-row: 4;
-      grid-column: 3 / 4;
-    }
-
-    & .six {
-      grid-row: 4 / 6;
-      grid-column: 4 / 6;
-    }
-
-    & .seven {
-      grid-row: 5;
-      grid-column: 1 / 4;
-    }
-  }
-}
-
-.home--contact {
-  padding: 5rem 0;
-}
-
-// page
-.home--about {
+  padding-top: 10rem;
   text-align: right;
   font-size: 1.8rem;
   background: var(--clr-dark);
@@ -204,50 +67,8 @@ export default class Home extends Vue {}
   }
 }
 
-.home__works {
-  &--container {
-    position: relative;
-    cursor: pointer;
-  }
-  &--image {
-    transition: all 0.5s;
-
-    &:hover {
-      opacity: 0.5;
-    }
-
-    &:hover + p.title,
-    &:hover + p.title + p.details {
-      opacity: 1;
-      transition: all 1s;
-    }
-
-    & + p.title {
-      position: absolute;
-      top: 3rem;
-      left: 3rem;
-      opacity: 0;
-      font-size: 2.5rem;
-      font-weight: 300;
-      pointer-events: none;
-      transition: all 0.5s;
-
-      & + p.details {
-        position: absolute;
-        top: 10rem;
-        left: 3rem;
-        right: 3rem;
-        opacity: 0;
-        font-size: 2rem;
-        font-weight: 400;
-        pointer-events: none;
-        transition: all 0.5s;
-      }
-    }
-  }
-}
-
 .home--contact {
+  padding: 5rem 0;
   text-align: center;
   background: #ef0d33;
 
@@ -279,7 +100,7 @@ export default class Home extends Vue {}
     }
 
     &:hover::after {
-      transform: translate(5px, 1px) scaleX(1.1);
+      transform: translate(5px, 1px) scale(1.1);
       border-bottom: 3px #47ffff solid;
     }
   }
