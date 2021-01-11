265b15d3a898d7fb79a1a46ad2fb76943d7477bd
Raaed Kabir <raaed.kabir@gmail.com>
2021-01-12 02:54:05 +0800

    added Stripe Checkout to About Page

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,38 @@
 <template>
   <main>
+    <AppModal class="modal" :open="showDialog" title="Buy Me a Coffee" @close="(showDialog = false), (amount = null)">
+      <template>
+        <div class="d-flex justify-between mt-3">
+          <div class="item">
+            <img src="@/assets/images/coffee/beans.svg" alt="coffee beans" />
+            <AppButton flat :class="{ selected: amount === 100 }" @click.native.prevent="amount = 100">$ 1</AppButton>
+          </div>
+          <div class="item">
+            <img src="@/assets/images/coffee/paper-cup.svg" alt="coffee cup" />
+            <AppButton flat :class="{ selected: amount === 500 }" @click.native.prevent="amount = 500">$ 5</AppButton>
+          </div>
+          <div class="item">
+            <img src="@/assets/images/coffee/machine.svg" alt="coffee machine" />
+            <AppButton flat :class="{ selected: amount === 2000 }" @click.native.prevent="amount = 2000"
+              >$ 20</AppButton
+            >
+          </div>
+          <div class="item">
+            <img src="@/assets/images/coffee/shop.svg" alt="coffee shop" />
+            <AppButton flat :class="{ selected: amount === 5000 }" @click.native.prevent="amount = 5000"
+              >$ 50</AppButton
+            >
+          </div>
+        </div>
+        <div class="d-flex justify-center">
+          <AppCard v-if="showStripe" :amount="amount" style="margin: 3rem 0 0" />
+        </div>
+        <div class="attribution">
+          Icons made by <AppLink href="https://www.flaticon.com/authors/srip" title="srip">srip</AppLink> from
+          <AppLink href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</AppLink>.
+        </div>
+      </template>
+    </AppModal>
     <section>
       <div class="container pt-12">
         <h1 class="heading__primary">About Me <span>_</span></h1>
@@ -17,7 +50,10 @@
               things and programming.
             </p>
             <br />
-            <p>If you'd like, click here to buy me a cup of coffee and help keep me awake.</p>
+            <p>
+              If you'd like, <span class="link" @click="showDialog = true">click here</span> to buy me a cup of coffee
+              and help keep me awake.
+            </p>
           </div>
           <div class="content--image">
             <!-- image goes here -->
@@ -27,6 +63,50 @@
     </section>
   </main>
 </template>
+
+<script>
+import AppCard from '@/components/AppCard.vue';
+import AppModal from '@/components/AppModal.vue';
+import AppButton from '@/components/AppButton.vue';
+import AppLink from '../components/AppLink.vue';
+
+export default {
+  components: {
+    AppCard,
+    AppButton,
+    AppModal,
+    AppLink,
+  },
+
+  data() {
+    return {
+      showDialog: false,
+      showStripe: false,
+      amount: null,
+    };
+  },
+
+  watch: {
+    amount(val, oldVal) {
+      if (val) this.showStripe = true;
+      else this.showStripe = false;
+
+      if (val && oldVal !== null) {
+        this.showStripe = false;
+        this.$nextTick(() => {
+          this.showStripe = true;
+        });
+      }
+    },
+  },
+
+  head() {
+    return {
+      script: [{ src: 'https://js.stripe.com/v3/' }],
+    };
+  },
+};
+</script>
 
 <style lang="scss" scoped>
 .content {
@@ -50,6 +130,33 @@
       font-weight: 700;
       color: #c8234a;
     }
+  }
+}
+
+.modal {
+  img {
+    height: 8rem;
+  }
+
+  .attribution {
+    text-align: right;
+    margin-top: 1em;
+    font-size: 1.4rem;
+    font-weight: 300;
+  }
+
+  .selected {
+    background: var(--clr-dark);
+  }
+}
+
+.item {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  img {
+    margin-bottom: 2rem;
   }
 }
 </style>
