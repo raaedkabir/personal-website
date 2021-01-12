65a7bade8f5a12615cc498324e8cc243b516bae4
Raaed Kabir <raaed.kabir@gmail.com>
2021-01-12 19:27:54 +0800

    added to Projects Page

---
+++ components/AppWorkPreview.vue
@@ -0,0 +1,56 @@
+<template>
+  <nuxt-link v-if="!outside" :to="to">
+    <img class="bg" :src="require('@/assets/images/works' + imgSrc)" :alt="imgAlt" />
+    <slot />
+  </nuxt-link>
+  <a v-else :href="to" target="_blank" rel="noopener noreferrer">
+    <img class="bg" :src="require('@/assets/images/works' + imgSrc)" :alt="imgAlt" />
+    <slot />
+  </a>
+</template>
+
+<script>
+// import AppButton from '@/components/UI/AppButton.vue';
+
+export default {
+  components: {
+    // AppButton,
+  },
+
+  props: {
+    to: {
+      type: String,
+      required: true,
+    },
+    imgSrc: {
+      type: String,
+      required: true,
+    },
+    imgAlt: {
+      type: String,
+      required: true,
+    },
+    outside: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+a {
+  padding: 0 2rem 2rem;
+  text-decoration: none;
+  color: inherit;
+
+  .bg {
+    margin-left: auto;
+    width: 90%;
+  }
+
+  button {
+    margin-left: 15%;
+  }
+}
+</style>
--- pages/works/index.vue
+++ pages/works/index.vue
@@ -3,13 +3,42 @@
     <section>
       <div class="container pt-12">
         <h1 class="heading__primary text-center">My Works <span>_</span></h1>
+
+        <div class="projects-container">
+          <AppWorkPreview
+            to="/blog/generative-art"
+            img-src="/generativeArt/heading.png"
+            img-alt="computer generated art"
+          >
+            <h2 class="heading__secondary">Generative Art with HTML5 Canvas</h2>
+            <AppButton>write-up</AppButton>
+          </AppWorkPreview>
+
+          <AppWorkPreview
+            outside
+            to="https://comecommune.netlify.app/"
+            img-src="/website/ComeCommune.png"
+            img-alt="website hero area"
+          >
+            <h2 class="heading__secondary">ComeCommune Blog Site</h2>
+            <AppButton>project</AppButton>
+          </AppWorkPreview>
+        </div>
       </div>
     </section>
   </main>
 </template>
 
 <script>
+import AppButton from '../../components/AppButton.vue';
+import AppWorkPreview from '../../components/AppWorkPreview.vue';
+
 export default {
+  components: {
+    AppButton,
+    AppWorkPreview,
+  },
+
   head() {
     return {
       title: 'My Works',
@@ -17,3 +46,25 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.projects-container {
+  display: flex;
+  flex-wrap: wrap;
+  width: 95%;
+  margin: 5rem auto 5rem;
+
+  button {
+    margin-left: 5%;
+    cursor: pointer;
+  }
+
+  > * {
+    flex: 0 50%;
+
+    @media only screen and (max-width: 700px) {
+      flex: 0 100%;
+    }
+  }
+}
+</style>
