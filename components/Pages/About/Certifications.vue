b6c9f8f91722c4d83d9236b2a9b308e5456a471b
Raaed Kabir <raaed.kabir@gmail.com>
2021-05-31 18:19:15 +0800

    feat: added Certifications section

---
+++ components/Pages/About/Certifications.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2 class="heading__secondary text-center">Certifications</h2>
+
+    <div class="badges mt-2">
+      <div
+        style="background: red"
+        data-iframe-width="150"
+        data-iframe-height="250"
+        data-iframe-background="blue"
+        data-share-badge-id="c42e9017-7863-442f-94e6-42d84d390038"
+        data-share-badge-host="https://www.credly.com"
+      ></div>
+      <script type="text/javascript" async src="//cdn.credly.com/assets/utilities/embed.js"></script>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.badges {
+  display: flex;
+  justify-content: center;
+
+  iframe {
+    background: #fff;
+  }
+}
+</style>
--- pages/about.vue
+++ pages/about.vue
@@ -8,6 +8,8 @@
 
         <Skills class="mt-6" />
 
+        <Certifications class="mt-6" />
+
         <div class="mt-6">
           <h2 class="heading__secondary text-center">Timeline</h2>
           <p class="text-center">Coming Soon</p>
@@ -19,11 +21,13 @@
 
 <script>
 import AboutMe from '@/components/Pages/About/AboutMe.vue';
+import Certifications from '@/components/Pages/About/Certifications.vue';
 import Skills from '@/components/Pages/About/Skills.vue';
 
 export default {
   components: {
     AboutMe,
+    Certifications,
     Skills,
   },
 
