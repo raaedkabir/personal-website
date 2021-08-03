bdfbe9b23a107d02a8caeca355317f74b4aa3faf
Raaed Kabir <raaed.kabir@gmail.com>
2021-08-03 12:03:06 +0800

    added AWS certifications

--- components/Pages/About/Certifications.vue
+++ components/Pages/About/Certifications.vue
@@ -2,15 +2,20 @@
   <div>
     <h2 class="heading__secondary text-center">Certifications</h2>
 
-    <div class="badges mt-2">
-      <div
-        style="background: red"
-        data-iframe-width="150"
-        data-iframe-height="250"
-        data-iframe-background="blue"
-        data-share-badge-id="c42e9017-7863-442f-94e6-42d84d390038"
-        data-share-badge-host="https://www.credly.com"
-      ></div>
+    <div class="badges">
+      <div 
+        data-iframe-width="150" 
+        data-iframe-height="250" 
+        data-share-badge-id="52eab62f-8692-4a9c-96e5-511f98ec5972" 
+        data-share-badge-host="https://www.credly.com">
+      </div>
+      <script type="text/javascript" async src="//cdn.credly.com/assets/utilities/embed.js"></script>
+      <div 
+        data-iframe-width="150" 
+        data-iframe-height="250" 
+        data-share-badge-id="2b14b75d-3595-4729-90ff-1b0bd9db059d" 
+        data-share-badge-host="https://www.credly.com">
+      </div>
       <script type="text/javascript" async src="//cdn.credly.com/assets/utilities/embed.js"></script>
     </div>
   </div>
@@ -23,7 +28,12 @@ export default {};
 <style lang="scss">
 .badges {
   display: flex;
-  justify-content: center;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+
+  > * {
+    margin-top: 2rem;
+  }
 
   iframe {
     background: #fff;
