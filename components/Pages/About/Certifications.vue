a327ba32bf2d8a788ebbd124015d44b5b75a7f92
Raaed Kabir <raaed.kabir@gmail.com>
2021-10-05 11:07:42 -0400

    fixed certifications

--- components/Pages/About/Certifications.vue
+++ components/Pages/About/Certifications.vue
@@ -3,20 +3,26 @@
     <h2 class="heading__secondary text-center">Certifications</h2>
 
     <div class="badges">
-      <div 
-        data-iframe-width="150" 
-        data-iframe-height="250" 
-        data-share-badge-id="52eab62f-8692-4a9c-96e5-511f98ec5972" 
-        data-share-badge-host="https://www.credly.com">
-      </div>
-      <script type="text/javascript" async src="//cdn.credly.com/assets/utilities/embed.js"></script>
-      <div 
-        data-iframe-width="150" 
-        data-iframe-height="250" 
-        data-share-badge-id="2b14b75d-3595-4729-90ff-1b0bd9db059d" 
-        data-share-badge-host="https://www.credly.com">
-      </div>
-      <script type="text/javascript" async src="//cdn.credly.com/assets/utilities/embed.js"></script>
+      <iframe
+        id="embedded-badge-52eab62f-8692-4a9c-96e5-511f98ec5972"
+        name="acclaim-badge"
+        allowtransparency="true"
+        scrolling="no"
+        src="//www.credly.com/embedded_badge/52eab62f-8692-4a9c-96e5-511f98ec5972"
+        style="width: 150px; height: 240px"
+        title="View my verified achievement on Credly."
+        frameborder="0"
+      ></iframe>
+      <iframe
+        id="embedded-badge-2b14b75d-3595-4729-90ff-1b0bd9db059d"
+        name="acclaim-badge"
+        allowtransparency="true"
+        scrolling="no"
+        src="//www.credly.com/embedded_badge/2b14b75d-3595-4729-90ff-1b0bd9db059d"
+        style="width: 150px; height: 240px"
+        title="View my verified achievement on Credly."
+        frameborder="0"
+      ></iframe>
     </div>
   </div>
 </template>
